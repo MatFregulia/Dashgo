08d81a29f9505478c086230d747d428c0df53798
Matheus Fregulia <matheus.gviva@gmail.com>
2022-03-11 09:44:39 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Matheus Fregulia</Text>
-                <Text color="gray.300" fontSize="small">
-                    mat.fregulia@gmail.com
-                </Text>
-            </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Matheus Fregulia</Text>
+                    <Text color="gray.300" fontSize="small">
+                        mat.fregulia@gmail.com
+                    </Text>
+                </Box>
+            ) }
 
             <Avatar size="md" name="Matheus Fregulia" src="https://github.com/MatFregulia.png" />
         </Flex>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useBreakpointValue } from '@chakra-ui/react'
 
 import { Logo } from '../Header/Logo';
 import { NotificationsNav } from '../Header/NotificationsNav';
@@ -6,14 +6,20 @@ import { Profile } from '../Header/Profile';
 import { SearchBox } from '../Header/SearchBox';
 
 export function Header() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Flex as="header" w="100%" maxWidth={1400} h="20" mx="auto" mt="4" px="6" align="center">
-            <Logo />     
-            <SearchBox />
+            <Logo />
+                
+            { isWideVersion && <SearchBox /> }
 
             <Flex align="center" ml="auto">
                 <NotificationsNav />
-                <Profile />
+                <Profile showProfileData={isWideVersion} />
             </Flex>
         </Flex>
     );
