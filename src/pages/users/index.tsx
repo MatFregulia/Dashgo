f7036488297e5f7e87f95f00e91ae63121f203b3
Matheus Fregulia <matheus.gviva@gmail.com>
2022-03-29 09:07:10 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,14 +6,26 @@ import { useQuery } from 'react-query'
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useEffect } from "react";
 
 export default function UserLIst() {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            };
+        });
+
+        return users;
     })
 
     const isWideVersion = useBreakpointValue({
@@ -67,29 +79,22 @@ export default function UserLIst() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Matheus Fregulia</Text>
-                                                <Text fontSize="sm" color="gray.300">mat.fregulia@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        { isWideVersion && <Td>08 de Março, 2022</Td> }
-                                        {/* <Td>
-                                            <Button 
-                                                as="a" 
-                                                size="sm" 
-                                                fontSize="sm" 
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                            >
-                                                { isWideVersion && Editar }
-                                            </Button>
-                                        </Td> */}
-                                    </Tr>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion && <Td>{user.createdAt}</Td> }
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
 
