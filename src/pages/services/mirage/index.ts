c194336243ba4277ca527d0d918e887f43851720
Matheus Fregulia <matheus.gviva@gmail.com>
2022-03-31 09:17:49 -0300

    Componente de paginação

--- src/pages/services/mirage/index.ts
+++ src/pages/services/mirage/index.ts
@@ -42,7 +42,6 @@ export function makeServer() {
 
                 const pageStart = (Number(page) - 1) * Number(per_page)
                 const pageEnd = pageStart + Number(per_page)
-                
 
                 const users = this.serialize(schema.all('user'))
                     .users.slice(pageStart, pageEnd)
