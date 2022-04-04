df3e5ed412f731767df01f8ff08aa9d01d6deabf
Matheus Fregulia <matheus.gviva@gmail.com>
2022-04-04 09:33:32 -0300

    Apagando imports não utilizados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,13 +5,12 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { getUsers, useUsers } from "../services/hooks/useUsers";
+import { useUsers } from "../services/hooks/useUsers";
 import { useState } from "react";
 import { queryClient } from "../services/queryClient";
 import { api } from "../services/api";
-import { GetServerSideProps } from "next";
 
-export default function UserList({ users }) {
+export default function UserList() {
     const [ page, setPage ] = useState(1);
     const { data, isLoading, isFetching, error } = useUsers(page)
 
